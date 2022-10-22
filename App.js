9d11f348a701800a865404e057c81d8e3a629924
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-10-22 16:42:05 +0900

    Navbar

--- App.js
+++ App.js
@@ -11,6 +11,13 @@ import {
   extendTheme,
   VStack,
   Box,
+  Image,
+  AspectRatio,
+  Stack,
+  Container,
+  StatusBar,
+  IconButton,
+  Icon,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
@@ -27,13 +34,98 @@ export const theme = extendTheme({ config });
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
+      <StatusBar bg="#3700B3" barStyle="light-content" />
+      <Box safeAreaTop bg="#3700B3" />
+      <HStack bg="violet.600" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%">
+        <HStack alignItems="center">
+          <Text color="white" fontSize="20" fontWeight="bold">
+            Home
+          </Text>
+        </HStack>
+      </HStack>
+      
+      {/* <Center
         _dark={{ bg: "blueGray.900" }}
         _light={{ bg: "blueGray.50" }}
         px={4}
         flex={1}
       >
-        <VStack space={5} alignItems="center">
+
+
+        <Container>
+            <Heading>
+              A component library for the
+              <Text color="emerald.500"> React Ecosystem</Text>
+            </Heading>
+            <Text mt="3" fontWeight="medium">
+              NativeBase is a simple, modular and accessible component library that
+              gives you building blocks to build you React applications.
+            </Text>
+        </Container>
+
+        <HStack space={3} justifyContent="center">
+          <Text>React</Text>
+          <Text>is</Text>
+          <Text>good</Text>
+        </HStack>
+
+        <Box alignItems="center">
+          <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
+          borderColor: "coolGray.600",
+          backgroundColor: "gray.700"
+        }} _web={{
+          shadow: 2,
+          borderWidth: 0
+        }} _light={{
+          backgroundColor: "gray.50"
+        }}>
+            <Box>
+              <AspectRatio width="100%" ratio={16 / 9}>
+                <Image source={{
+                uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg"
+              }} alt="image" />
+              </AspectRatio>
+              <Center bg="violet.500" _dark={{
+              bg: "violet.400"
+            }} _text={{
+              color: "warmGray.50",
+              fontWeight: "700",
+              fontSize: "xs"
+            }} position="absolute" bottom="0" px="3" py="1.5">
+                PHOTOS
+              </Center>
+            </Box>
+            <Stack p="4" space={3}>
+              <Stack space={2}>
+                <Heading size="md" ml="-1">
+                  The Garden City
+                </Heading>
+                <Text fontSize="xs" _light={{
+                color: "violet.500"
+              }} _dark={{
+                color: "violet.400"
+              }} fontWeight="500" ml="-0.5" mt="-1">
+                  The Silicon Valley of India.
+                </Text>
+              </Stack>
+              <Text fontWeight="400">
+                Bengaluru (also called Bangalore) is the center of India's high-tech
+                industry. The city is also known for its parks and nightlife.
+              </Text>
+              <HStack alignItems="center" space={4} justifyContent="space-between">
+                <HStack alignItems="center">
+                  <Text color="coolGray.600" _dark={{
+                  color: "warmGray.200"
+                }} fontWeight="400">
+                    6 mins ago
+                  </Text>
+                </HStack>
+              </HStack>
+            </Stack>
+          </Box>
+        </Box> */}
+            
+        {/* <VStack space={5} alignItems="center">
           <NativeBaseIcon />
           <Heading size="lg">Welcome to NativeBase</Heading>
           <HStack space={2} alignItems="center">
@@ -60,8 +152,8 @@ export default function App() {
             </Text>
           </Link>
           <ToggleDarkMode />
-        </VStack>
-      </Center>
+        </VStack> */}
+      {/* </Center> */}
     </NativeBaseProvider>
   );
 }
