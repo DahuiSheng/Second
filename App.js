aff65b53a004164ac5e09188e71430510323f12a
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-10-22 18:30:45 +0900

    Add HomeScreen

--- App.js
+++ App.js
@@ -1,62 +1,60 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+
 import {
   Text,
-  Link,
   HStack,
-  Center,
-  Heading,
   Switch,
   useColorMode,
   NativeBaseProvider,
   extendTheme,
-  VStack,
-  Box,
-  Image,
-  AspectRatio,
-  Stack,
-  Container,
-  StatusBar,
-  IconButton,
-  Icon,
 } from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
-import { Platform } from "react-native";
 import Appbar from "./components/Appbar"
 import Footer from "./components/Footer";
 import Introduce from "./components/Introduce";
 
 // Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
+// const config = {
+//   useSystemColorMode: false,
+//   initialColorMode: "dark",
+// };
 
 // extend the theme
-export const theme = extendTheme({ config });
+// export const theme = extendTheme({ config });
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Appbar />
-      <Introduce/>
-      <Footer />
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Stack.Navigator>
+          <Stack.Screen name = "Home" component={HomeScreen} />
+        </Stack.Navigator>
+        {/* <Appbar />
+        <Introduce/>
+        <Footer /> */}
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
 
 // Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
+// function ToggleDarkMode() {
+//   const { colorMode, toggleColorMode } = useColorMode();
+//   return (
+//     <HStack space={2} alignItems="center">
+//       <Text>Dark</Text>
+//       <Switch
+//         isChecked={colorMode === "light"}
+//         onToggle={toggleColorMode}
+//         aria-label={
+//           colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+//         }
+//       />
+//       <Text>Light</Text>
+//     </HStack>
+//   );
+// }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "native-base"
+
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>ホーム画面</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
